317d0da9b03cc6dd5b1127729c7e30ed22985148
kolabank <kolabank@gmail.com>
2022-01-05 07:09:29 +0100

    Eureka Server set up and working

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,10 +1,13 @@
 package com.udacity.pricing.domain.price;
 
+import javax.persistence.Entity;
 import java.math.BigDecimal;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+
+
 public class Price {
 
     private String currency;
